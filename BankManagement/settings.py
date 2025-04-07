983b698a90fe1fa336d82207083e98d2f2a18ecc
Arpit-Agrawal-27 <arpitagrawal404@gmail.com>
2025-04-07 09:45:31 +0530

    some Change

--- BankManagement/settings.py
+++ BankManagement/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'BankManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Pigibank',
-        'USER': 'root',
-        'PASSWORD': 'Arpit@123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 
 # Password validation
